9c0028d61d563a31fb9feb64a74cc07309c7b04f
harshinihaze <karnatiharshini2025@gmail.com>
2026-06-13 19:23:17 +0530

    Create Menu.jsx

---
+++ src/src/src/components/src/components/Menu.jsx
@@ -0,0 +1,15 @@
+export default function Menu() {
+  return (
+    <section className="menu">
+      <h2>Signature Menu</h2>
+      <ul>
+        <li>Butter Garlic Prawns</li>
+        <li>Burmese Roll</li>
+        <li>Pasta</li>
+        <li>Tiramisu</li>
+        <li>Latte</li>
+        <li>Mojito</li>
+      </ul>
+    </section>
+  )
+}
