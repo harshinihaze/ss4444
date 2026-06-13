03f1d01fc3f38fbca5a29fe02590ec673b842dd1
harshinihaze <karnatiharshini2025@gmail.com>
2026-06-13 19:22:29 +0530

    Create Hero.jsx

---
+++ src/src/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <h1>SkySalt Cafe & Bistro</h1>
+      <p>Rooftop vibes above Hyderabad skyline</p>
+
+      <div className="buttons">
+        <button>Reserve Table</button>
+        <button>View Menu</button>
+      </div>
+    </section>
+  )
+}
