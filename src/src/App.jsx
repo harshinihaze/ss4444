f47c8e481b96917b782fc521b134e20f675e2e5b
harshinihaze <karnatiharshini2025@gmail.com>
2026-06-13 19:21:46 +0530

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero"
+import Menu from "./components/Menu"
+import Games from "./components/Games"
+import Footer from "./components/Footer"
+
+export default function App() {
+  return (
+    <div>
+      <Hero />
+      <Menu />
+      <Games />
+      <Footer />
+    </div>
+  )
+}
